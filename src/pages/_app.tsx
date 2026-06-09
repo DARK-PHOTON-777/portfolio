4cf25a1477f26cced580ade6cc2d5ae526d9b0e5
RENO WARNER <reno.warner.777@gmail.com>
2026-06-09 00:15:30 -0400

    FEAT: Inserted title and description head tags.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Reno Warner | Portfolio</title>
+				<meta
+					name="description"
+					content="Portfolio of Reno Warner - System Engineer"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
